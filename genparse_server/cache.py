164fe89372bc659cf6b9982ec7b7943deb3763f6
benlebrun <benlebrun1@gmail.com>
2024-07-25 20:59:27 +0000

    actual cache file

---
+++ genparse_server/cache.py
@@ -0,0 +1,61 @@
+from collections import deque
+from util import load_proposal, make_guide
+import logging
+
+class ProposalCache:
+    def __init__(self, maxsize=10):
+        self.maxsize = maxsize
+        self.cache = {}
+        self.recent_keys = deque(maxlen=10)
+
+    def make_cache_key(self, request):
+        if len(request['proposal_args']) == 0:
+            return (
+                request['lark_grammar'],
+                request['proposal_name']
+            )
+        elif len(request['proposal_args']) == 1 and 'K' in request['proposal_args']:
+            return (
+                request['lark_grammar'],
+                request['proposal_name'],
+                request['proposal_args']['K']
+            )
+        else:
+            raise ValueError('Invalid proposal arguments.')
+    
+    def fetch_or_create_proposal(self, request, llm):
+        key = self.make_cache_key(request)
+        if key in self.cache:
+            logging.info('Reusing cached parallel proposal')
+            self.recent_keys.append(key)
+            return self.cache[key]
+        else:
+            logging.info('Creating new parallel proposal')
+
+            guide = make_guide(request['lark_grammar'])
+            parallel_proposal = load_proposal(
+                request['proposal_name'], llm, guide, request['proposal_args']
+            )
+    
+            self.cache[key] = parallel_proposal
+            self.recent_keys.append(key)
+            
+            if len(self.cache) > self.maxsize:
+                logging.info('Evicting old parallel proposal')
+                self.evict_objects()
+            
+            return parallel_proposal
+    
+    def evict_objects(self):
+        keys_to_remove = set(self.cache.keys()) - set(self.recent_keys)
+        
+        for key in keys_to_remove:
+            self.cache[key].cleanup()
+            del self.cache[key]
+    
+    def clear_cache(self):
+        self.cache.clear()
+        self.recent_keys.clear()
+    
+    def __repr__(self):
+        return f"ProposalCache(maxsize={self.maxsize}, current_size={len(self.cache)})"
